0e5fb926a48372b83730a75aa59f30b1e7b59d23
Luis Miguel <lucho7030@gmail.com>
2017-11-14 22:29:47 -0500

    Arreglar navbar fixed to the top

--- src/components/navigation/Navbar.js
+++ src/components/navigation/Navbar.js
@@ -3,18 +3,18 @@ import { Link } from 'react-router-dom';
 
 
 const Navbar = () => (
-  <nav className="navbar navbar-expand-lg navbar-light bg-light">
-    <Link className="navbar-brand" to="/">Messier Solutions</Link>
+  <nav className="navbar fixed-top navbar-expand-lg navbar-light blue-grey lighten-5">
+    <Link to="/"><div className="navbar-brand">Messier Solutions</div></Link>
     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
 
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav mr-auto">
-        <li className="nav-item active">
+        <li className="nav-item">
           <a className="nav-link">Home <span className="sr-only">(current)</span></a>
         </li>
-        <li className="nav-item active">
+        <li className="nav-item ">
           <Link className="nav-link" to="/blog">Blog</Link>
         </li>
       </ul>
