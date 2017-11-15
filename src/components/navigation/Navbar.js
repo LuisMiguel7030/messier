d0ceac7f7ae49921aa8a90c48fe1bae48cc405e9
Luis Miguel <lucho7030@gmail.com>
2017-11-14 23:45:33 -0500

    new color button for search and parallax buttons

--- src/components/navigation/Navbar.js
+++ src/components/navigation/Navbar.js
@@ -20,7 +20,7 @@ const Navbar = () => (
       </ul>
       <form className="form-inline my-2 my-lg-0">
         <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button className="btn btn-elegant" type="submit">Search</button>
+        <button className="btn btn-mdb" type="submit">Search</button>
       </form>
     </div>
   </nav>
