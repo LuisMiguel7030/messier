9848a5679b25b26b0c40865b5a26d5d392771202
klaus <klaus.super@gmail.com>
2017-12-06 19:39:08 -0500

    Poniendo algo nuevo en las tarjetas

--- src/components/contentpages/CardMVP.js
+++ src/components/contentpages/CardMVP.js
@@ -10,7 +10,7 @@ const CardMVP = () => (
         <div className="card-body">
           <h4 className="card-title">Misión</h4>
           <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
-          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <p className="card-text">Ejemplooooo of the card's content.</p>
           <a className="card-link">Card link</a>
           <a className="card-link">Another link</a>
         </div>
